6ccc6eb4078903b48caf1e9d6589b66a67561d26
Alden Perrine <aldenperrine@gmail.com>
2020-03-05 13:21:44 -0800

    FC working

--- models.py
+++ models.py
@@ -9,11 +9,16 @@ from tensorflow.keras.mixed_precision import experimental as mixed_precision
 
 # inputs: a keras.Input
 # layers: set of layers built on top of the input
-def make_mixed_model(x_train, y_train, x_test, y_test):
+def make_fc_model(x_train, y_train, x_test, y_test):
+    x_train = x_train.reshape(2115, -1)
+    x_test = x_test.reshape(443, -1)
+    y_train -= 769
+    y_test -= 769
+
     policy = mixed_precision.Policy('mixed_float16')
     mixed_precision.set_policy(policy)
     num_units = 4096
-    inputs = keras.Input(shape=(22, 1000, ), name='eeg_data')
+    inputs = keras.Input(shape=(22000, ), name='eeg_data')
     dense1 = layers.Dense(num_units, activation='relu', name='dense_1')
     x = dense1(inputs)
     dense2 = layers.Dense(num_units, activation='relu', name='dense_2')
@@ -26,9 +31,9 @@ def make_mixed_model(x_train, y_train, x_test, y_test):
                   optimizer=keras.optimizers.RMSprop(),
                   metrics=['accuracy'])
     history = model.fit(x_train, y_train,
-                    batch_size=100,
-                    epochs=5,
-                    validation_split=0.2)
+                        batch_size=10,
+                        epochs=10,
+                        validation_split=0.1)
     test_scores = model.evaluate(x_test, y_test, verbose=2)
     print('Test loss:', test_scores[0])
     print('Test accuracy:', test_scores[1])
